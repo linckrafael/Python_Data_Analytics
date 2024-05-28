ab9a85feef9bf2f5bf1ce02de256cab73f411f16
linckrafael <rafaelinck@hotmail.com>
2024-05-28 16:52:52 -0300

    First commit

---
+++ Python/Exercicios/desafio2.py
@@ -0,0 +1,5 @@
+valor = float(input())
+
+if valor > 0:
+    #TODO: Imprimir a mensagem de sucesso, formatando o saldo atual (vide Exemplos).
+    print(f"Deposito realizado com sucesso! \n Saldo atual: R$ {valor:.2f}")
---
+++ Python/Exercicios/primeiroPrograma.py
@@ -0,0 +1,6 @@
+print("Hello World")
+print(5/2)
+
+carros = ("gol")
+print(isinstance(carros, tuple))
+
